fe1ae46bac3c32949d7e7e129b6bdbb41462e7dd
amrElzohairy <aelzohairy9@gmail.com>
2025-02-09 16:01:37 +0200

    Add AddNoteBottomSheet and update NotesView
    - Added a new `AddNoteBottomSheet` widget.
    - Updated `NotesView` to include a transparent AppBar background.
    - Implemented functionality to show `AddNoteBottomSheet` when the FloatingActionButton is pressed.

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,6 +9,7 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         titleSpacing: 22,
         title: const Text("Notes"),
         actionsPadding: const EdgeInsets.symmetric(horizontal: 16),
@@ -17,7 +19,14 @@ class NotesView extends StatelessWidget {
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(8.0),
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              },
+            );
+          },
           backgroundColor: Colors.lightBlueAccent,
           shape: const CircleBorder(),
           child: const Icon(Icons.add),
