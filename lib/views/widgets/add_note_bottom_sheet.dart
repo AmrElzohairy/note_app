a8f6c4791f407bbc095a523bc6a8f497921ffc12
amrElzohairy <aelzohairy9@gmail.com>
2025-02-09 16:14:43 +0200

    Add constants and custom widgets for note app
    Created a new constants file to store app-wide constants such as colors and box names. Updated the AddNoteBottomSheet to include custom text fields and a button. Added new custom widgets, CustomButton and CustomTextField, to enhance the UI components of the note app.

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_button.dart';
+import 'package:note_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.only(left: 16, right: 16, top: 50),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextField(hint: 'Title', maxLines: 1),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'Content', maxLines: 5),
+            SizedBox(height: 32),
+            CustomButton(),
+            SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
   }
 }
