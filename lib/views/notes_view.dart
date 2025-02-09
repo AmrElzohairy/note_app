50c864c557d48de929e6150ddedb396c6a42c25b
amrElzohairy <aelzohairy9@gmail.com>
2025-02-09 15:13:58 +0200

    Add NotesView and NotesViewBody widgets
    Implemented the `NotesView` widget with an AppBar and a body containing `NotesViewBody`. Updated `main.dart` to use `NotesView` as the home screen and adjusted the theme settings.

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/notes_view_body.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 22,
+        title: const Text("Notes"),
+        actionsPadding: const EdgeInsets.symmetric(horizontal: 16),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
+      body: const SafeArea(child: NotesViewBody()),
+    );
+  }
+}
