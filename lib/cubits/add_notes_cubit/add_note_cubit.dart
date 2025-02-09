82587ba8b27f187de8fb5ba379a332c350522f72
amrElzohairy <aelzohairy9@gmail.com>
2025-02-09 21:32:51 +0200

    Add functionality to add notes using Hive and Bloc
    Implemented the `addNote` method in `AddNoteCubit` to add notes to a Hive box. Updated `main.dart` to include `MultiBlocProvider` for `AddNoteCubit` to manage the state of adding notes.

--- lib/cubits/add_notes_cubit/add_note_cubit.dart
+++ lib/cubits/add_notes_cubit/add_note_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteLoading());
+      await noteBox.add(note);
+    } catch (e) {
+      emit(AddNoteFailure(errorMessage: e.toString()));
+    }
+  }
 }
